a407c58d43ed63b464d88028863e7cbc9bbad4d1
hayrunnisayilbur <145451185+hayrunnisayilbur@users.noreply.github.com>
2023-11-21 14:00:38 +0300

    Add files via upload

---
+++ while döngüsü ödev1-847/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace while_döngüsü_ödev1_847
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbSayilar1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lbSayilar1
+            // 
+            this.lbSayilar1.FormattingEnabled = true;
+            this.lbSayilar1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.lbSayilar1.Location = new System.Drawing.Point(85, 49);
+            this.lbSayilar1.Name = "lbSayilar1";
+            this.lbSayilar1.Size = new System.Drawing.Size(122, 225);
+            this.lbSayilar1.TabIndex = 0;
+            this.lbSayilar1.SelectedIndexChanged += new System.EventHandler(this.lbSayilar1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbSayilar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbSayilar1;
+    }
+}
+
---
+++ while döngüsü ödev1-847/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace while_döngüsü_ödev1_847
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lbSayilar1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int sayi = 1;
+            while( sayi <= 10)
+            {
+                lbSayilar1.Items.Add(sayi);
+                sayi ++;
+            }
+        }
+    }
+}
